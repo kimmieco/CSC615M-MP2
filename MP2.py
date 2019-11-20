39d4312f7b1ef1b1b78cf6d2d3de7ba52dc68dc4
kimmieco <kimberly_jane_co@dlsu.edu.ph>
2019-11-20 14:41:21 +0800

    editted

--- MP2.py
+++ MP2.py
@@ -1,4 +1,4 @@
-input = "L={ak c bk|k>=1}"
+input = "L={ak c bm d|k>=1,m>2}"
 
 input = input[3:-1]
 
@@ -7,21 +7,18 @@ string = input.split("|")
 input_alphabet = []
 
 conditions = string[1].split(",")
-for cond in conditions:
-	inputdict = { "superscript":cond[0],"elements":[],"condition": cond[1:]}
-	input_alphabet.append(inputdict)
-# print(input_alphabet)
-input_alphabet.append({ "superscript":"1","elements":[]})
-elements = string[0].split(" ")
-for element in elements:
-	for alpha in input_alphabet:
 
+elements = string[0].split(" ")
+
+for element in elements:
+	for cond in conditions:
 		if len(element) == 2:
-			if alpha["superscript"] == element[1]:
-				alpha["elements"].append({element[0]:0})
-		else:
-			if alpha["superscript"] == "1":
-				alpha["elements"].append({element[0]:1})
-	
+			print(element[0])
+			if element[1] == cond[0]:
+				inputdict = {"superscript":element[1], "element":element[0], "condition": cond[1:]} #superscript:character
+				input_alphabet.append(inputdict)
+	if len(element) == 1:
+		inputdict = {"superscript":"1", "element":element[0]} #one character
+		input_alphabet.append(inputdict)
 print(input_alphabet)
 
