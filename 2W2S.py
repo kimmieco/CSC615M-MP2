b6900038a54ca56a9047645cf1c76ad7c438b18c
Daniel Lachica <daniel.lachica82@gmail.com>
2019-11-27 17:50:22 +0800

    Update 2W2S.py parser

--- 2W2S.py
+++ 2W2S.py
@@ -1,33 +1,108 @@
-s = "scan"
-w = "write"
-r = "read"
+import re
+
+# define constants
+sr = "scan R"
+sl = "scan L"
+w1 = "write 1"
+w2 = "write 2"
+r1 = "read 1"
+r2 = "read 2"
+h = "halt"
+
+# define classes
+class State:
+	def __init__(self, number, action):
+		self.number = number
+		self.action = action
+		self.transitions = []
+
+	def add_transition(self, input, next_state):
+		self.transitions.append(Transition(input, next_state))
+
+class Transition:
+	def __init__(self, input, next_state):
+		self.input = input
+		self.next_state = next_state
+
+class Stack:
+	def __init__(self):
+		self.is_empty = True
+		self.used = False
+		self.superscript = None
+		self.symbol = None
+
 
 '''
-Creates program step in format:
+Creates a state in the PDA:
 
 <curr_state>]<action>(<input>, <next_state>)
 '''
 def make_step(curr_state, action, input, next_state):
-  return str(curr_state) + "] " + action + "(" + input + "," + str(next_state) + ")"
+	state = State(curr_state, action)
+	if(input is not None and next_state is not None):
+		state.add_transition(input, next_state)
+	return state
 
 '''
-Appends an additional (<input>, <next_state>) to a program step
+Returns most recently added scan state in the program 
 '''
-def add_to_step(step, input, next_state):
-  step = step + "(" + input + "," + str(next_state) + ")"
-  return step
+def get_latest_scan_state(program):
+	for state in reversed(program):
+		#print(i)
+		#state = program[i]
+		if(state.action[0] == s):
+			#print("Latest state: " + str(state.number))
+			return state
+	return None
+
+def find_same_superscript(input_alphabet, superscript, start, dir):
+	for i in range(dir, len(input_alphabet)):
+		if(input_alphabet[i]['superscript'] == superscript):
+			return input_alphabet[i]
+	return None
+
+def print_program(program):
+	for state in program:
+		line = str(state.number) + "]" + state.action
+		for transition in state.transitions:
+			next_state = transition.next_state
+			line += "(" + transition.input + "," + str(next_state.number) + ")"
+		print(line)
+
+def push_stack(stack, superscript, symbol):
+	stack.is_empty = False
+	stack.used = True
+	stack.superscript = superscript
+	stack.symbol = symbol
+
+def pop_stack(stack):
+	stack.is_empty = True
+	stack.superscript = None
+	stack.symbol = None
 
 def generate_program(input_alphabet):
-  
-  program = [] # Set of program instructions
+	
+	program = [] # Set of program instructions
 
-  curr_state = 1 # Current state in the program
+	curr_state = 1 # Current state in the program
 
-  step = make_step(curr_state, s, '#', curr_state)
-  step = add_to_step(step, '#', curr_state+1)
-  curr_state += 1 # Move to next state
+	# Instantiate stacks
+	stack1 = Stack()
+	stack2 = Stack()
 
-  print(step)
+	# Add first step in program
+	state = make_step(curr_state, sr, None, None)
+	curr_state += 1
+	state.add_transition('#', state)
+	next_state = State(curr_state, sr)
+	curr_state += 1
+	state.add_transition('#', next_state)
+	program.append(state)
+
+	# Iterate through every token in the language
+
+	print_program(program)
+	return program
 
 
 if __name__ == "__main__":
